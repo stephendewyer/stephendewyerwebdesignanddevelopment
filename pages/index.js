8b31a321bd85d33fe6d36b4c11e067e02e4eac50
stephendewyer <stephen.dewyer@stephengdewyer.info>
2021-10-29 20:41:54 -0500

    first deployment

---
+++ pages/index.js
@@ -0,0 +1,90 @@
+import Head from 'next/head';
+import React, { Fragment } from 'react';
+import VideoJS from '../components/videoJS/videoJS' // point to where the functional component is stored
+import styles from '../styles/Index.module.css';
+
+const IndexPage = () => {
+
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = { // lookup the options in the docs for more options
+    autoplay: true,
+    loop: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    poster: '/videos/imagine_echo/imagine_echo_still_02.jpg',
+    sources: [{
+      src: '/videos/imagine_echo/imagine_echo.mp4',
+      type: 'video/mp4'
+    }]
+  }
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // you can handle player events here
+    player.on('waiting', () => {
+      console.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      console.log('player will dispose');
+    });
+  };
+
+  return (
+    <Fragment>
+      <Head>
+        <title>stephen dewyer web design and development</title>
+        <meta name="description" content="portfolio of web design and development by stephen garrett dewyer" />
+
+      </Head>
+      <div className="container">
+        <div className="header">
+          web design and development
+        </div>
+        <div className={styles.intro_video}>
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        </div>
+        <div className="paragraphs_container">
+          <div className="paragraphs">
+              stephen garrett dewyer is a:
+              <ul><br/>
+                <li>
+                  full-stack web developer;
+                </li><br/>
+                <li>
+                  User Interaction (U.I.) and User Experience (U.X.) designer;
+                </li><br/>
+                <li>
+                  award-winning artist whose work has shown in national and international exhibitions;
+                </li><br/>
+                <li>
+                  organizer with a history of bringing diverse parts of the community together to produce critically-acclaimed projects.
+                </li><br/>
+              </ul>
+              He created digital designs including web programs for:
+              <ul><br/>
+                <li>
+                  creative organizations;
+                </li><br/>
+                <li>
+                  progressive campaigns and movements.
+                </li><br/>
+              </ul>
+              He enjoys:
+              <ul><br/>
+                <li>
+                  using advancements in art and digital technology to improve design for the public and businesses.
+                </li><br/>
+              </ul>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+    
+  )
+}
+
+export default IndexPage;
